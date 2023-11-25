1ca8dd72bd959eef9b21869d603a47ee8e39f54f
Jeikhun Jalilov <jeykhunvj@code.edu.az>
2023-11-25 07:42:39 +0400

    edited: Pages

--- JS/fag.js
+++ JS/fag.js
@@ -12,3 +12,25 @@
 //   </StrictMode>,
 //   root
 // );
+
+let questionBlock = document.querySelectorAll(".question");
+questionBlock.forEach(element => {
+    element.addEventListener("click", (e)=>{
+        
+        if(element.firstElementChild.lastElementChild.style.display == "none" || element.firstElementChild.lastElementChild.style.display == ""){
+            
+            element.lastElementChild.firstElementChild.style.display = "none";
+            element.lastElementChild.lastElementChild.style.display = "initial";
+            element.style.background = "#FFF6F3";
+            element.style.filter = "drop-shadow(0px 4px 9px rgba(229, 100, 65, 0.13));";
+            element.firstElementChild.lastElementChild.style.display = "initial";
+        }else{
+            
+            element.lastElementChild.firstElementChild.style.display = "initial";
+            element.lastElementChild.lastElementChild.style.display = "none";
+            element.style.background = "#FFF";
+            element.style.filter = "drop-shadow(0px 4px 9px rgba(230, 230, 230, 0.25))";
+            element.firstElementChild.lastElementChild.style.display = "none";
+        }
+    })
+});
